1a4dc89ac98cc7287d08b9fa3d9c82eb309d18f3
Rory Trunkhill <rtrunkhill@gmail.com>
2017-10-04 16:52:20 +0000

    test email succesful!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   
   # has_many :wikis
   
